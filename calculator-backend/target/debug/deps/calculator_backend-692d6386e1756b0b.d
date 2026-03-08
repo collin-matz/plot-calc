4373bb8a46c9185e3bb3f03a7a9a6cbb1f33a272
Collin Matz <collin.matz.a@gmail.com>
2026-03-07 18:49:59 -0600

    added rust project for backend and react project for frontend

---
+++ calculator-backend/target/debug/deps/calculator_backend-692d6386e1756b0b.d
@@ -0,0 +1,5 @@
+C:\Users\colli\repos\plot-calc\calculator-backend\target\debug\deps\calculator_backend-692d6386e1756b0b.d: src\main.rs
+
+C:\Users\colli\repos\plot-calc\calculator-backend\target\debug\deps\libcalculator_backend-692d6386e1756b0b.rmeta: src\main.rs
+
+src\main.rs:
---
+++ calculator-backend/target/debug/deps/calculator_backend-fc422803d2dd4e33.d
@@ -0,0 +1,5 @@
+C:\Users\colli\repos\plot-calc\calculator-backend\target\debug\deps\calculator_backend-fc422803d2dd4e33.d: src\main.rs
+
+C:\Users\colli\repos\plot-calc\calculator-backend\target\debug\deps\libcalculator_backend-fc422803d2dd4e33.rmeta: src\main.rs
+
+src\main.rs:
